aae9b302d81d91caf46acc72a7b766695bf14b72
Johan S. <johsve@protonmail.com>
2024-01-17 11:03:00 +0100

    Updated

--- sandbox.js
+++ sandbox.js
@@ -1,7 +1,33 @@
-var createNinja = function (name, belt, age) { return ({
-    name: name,
-    belt: belt,
-    age: age,
-}); };
-var ninja1 = createNinja('Ryu', 'Black', 30);
-console.log(ninja1);
+/* interface Ninja {
+  name: string;
+  belt: string;
+  age: number;
+}
+
+const createNinja = (name: string, belt: string, age: number): Ninja => ({
+  name,
+  belt,
+  age,
+});
+
+let ninja1 = createNinja('Ryu', 'Black', 30);
+
+console.log(ninja1); */
+var character;
+var age;
+var isLoggedIn;
+age = 30;
+isLoggedIn = true;
+/*
+This is how you make an array of strings, there is a difference in vanilla js when you just do it like
+let ninjas = [];
+*/
+var ninjas = [];
+ninjas[1] = 'John';
+console.log(ninjas);
+ninjas.push('Mike');
+console.log(ninjas);
+ninjas.push(1);
+console.log(ninjas);
+ninjas.push(true);
+console.log(ninjas);
--- sandbox.ts
+++ sandbox.ts
@@ -1,4 +1,4 @@
-interface Ninja {
+/* interface Ninja {
   name: string;
   belt: string;
   age: number;
@@ -12,4 +12,33 @@ const createNinja = (name: string, belt: string, age: number): Ninja => ({
 
 let ninja1 = createNinja('Ryu', 'Black', 30);
 
-console.log(ninja1);
+console.log(ninja1); */
+
+let character: string;
+let age: number;
+let isLoggedIn: boolean;
+
+age = 30;
+isLoggedIn = true;
+
+/* 
+This is how you make an array of strings, there is a difference in vanilla js when you just do it like 
+let ninjas = []; 
+*/
+let ninjas: (string | number | boolean)[] = [];
+
+ninjas[1] = 'John';
+console.log(ninjas);
+ninjas.push('Mike');
+console.log(ninjas);
+ninjas.push(1);
+console.log(ninjas);
+ninjas.push(true);
+console.log(ninjas);
+
+/*
+Union type "(number | string)[]" används bara när den är framför en array men om de inte är en array så behövs inte union type, exempel neranför.
+*/
+let uid: string | number;
+uid = '123';
+uid = 123;
