f68d6985ec3fae0acc62560bdf360f94a7044eaf
Johan S. <johsve@protonmail.com>
2024-01-17 10:10:12 +0100

    Updated

--- sandbox.ts
+++ sandbox.ts
@@ -4,8 +4,8 @@ let isBlackBelt = false;
 
 character = '32';
 
-const circ = (diameter:number) => {
+const circ = (diameter: number) => {
   return diameter * Math.PI;
 };
 
-console.log(circ(12)):
+console.log(circ(12));
