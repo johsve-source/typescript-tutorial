3462241ba92a5a460398d8acbb3e557b812de7b8
Johan S. <johsve@protonmail.com>
2024-01-17 10:09:27 +0100

    Updated

--- sandbox.js
+++ sandbox.js
@@ -2,3 +2,7 @@ var character = 'luigi';
 var age = 30;
 var isBlackBelt = false;
 character = '32';
+var circ = function (diameter) {
+    return diameter * Math.PI;
+};
+console.log(circ(12));
--- sandbox.ts
+++ sandbox.ts
@@ -3,3 +3,9 @@ let age = 30;
 let isBlackBelt = false;
 
 character = '32';
+
+const circ = (diameter:number) => {
+  return diameter * Math.PI;
+};
+
+console.log(circ(12)):
