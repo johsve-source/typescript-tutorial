0ebd571b854e1f0d4477bfb3dace7eb2de81a017
Johan S. <johsve@protonmail.com>
2024-01-17 10:14:40 +0100

    Updated

--- sandbox.js
+++ sandbox.js
@@ -4,3 +4,8 @@ console.log(names[3]);
 var numbers = [10, 20, 30, 40];
 numbers.push(50);
 console.log(numbers);
+var mixed = ['ken', 4, 'chun-li', 8, 9];
+mixed.push('barbie');
+console.log(mixed);
+mixed.push(20);
+console.log(mixed);
--- sandbox.ts
+++ sandbox.ts
@@ -6,3 +6,10 @@ console.log(names[3]);
 let numbers = [10, 20, 30, 40];
 numbers.push(50);
 console.log(numbers);
+
+let mixed = ['ken', 4, 'chun-li', 8, 9];
+
+mixed.push('barbie');
+console.log(mixed);
+mixed.push(20);
+console.log(mixed);
