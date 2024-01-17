51dde9913ad1f404d67c51b96d9b6d0c5051e453
Johan S. <johsve@protonmail.com>
2024-01-17 10:13:38 +0100

    Updated

--- sandbox.js
+++ sandbox.js
@@ -1,3 +1,6 @@
 var names = ['luigi', 'mario', 'yoshi'];
 names.push('toad');
 console.log(names[3]);
+var numbers = [10, 20, 30, 40];
+numbers.push(50);
+console.log(numbers);
--- sandbox.ts
+++ sandbox.ts
@@ -2,3 +2,7 @@ let names = ['luigi', 'mario', 'yoshi'];
 
 names.push('toad');
 console.log(names[3]);
+
+let numbers = [10, 20, 30, 40];
+numbers.push(50);
+console.log(numbers);
