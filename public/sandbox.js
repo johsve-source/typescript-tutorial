d51882239a3a1d582511985a3476748b6542ad04
Johan S. <johsve@protonmail.com>
2024-01-18 13:43:57 +0100

    Update

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,4 +1,11 @@
 "use strict";
 let character = 'maria';
 console.log(character);
+const car = {
+    id: -1,
+    start: (id) => {
+        console.log(`The car ${id} is running!`);
+    },
+};
+console.log(car.start(0));
 //# sourceMappingURL=sandbox.js.map
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,2 +1,11 @@
 let character: string = 'maria';
 console.log(character);
+
+const car = {
+  id: -1,
+  start: (id: number) => {
+    console.log(`The car ${id} is running!`);
+  },
+};
+
+console.log(car.start(0));
